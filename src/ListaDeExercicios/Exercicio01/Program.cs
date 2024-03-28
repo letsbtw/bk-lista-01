5e04b3c0fee67fef465583375b19db756f4a4550
Leticia Cavalheiro <leticiascavalheiro@gmail.com>
2024-03-28 19:46:50 -0300

    Exercício 01.

--- src/ListaDeExercicios/Exercicio01/Program.cs
+++ src/ListaDeExercicios/Exercicio01/Program.cs
@@ -10,8 +10,23 @@
                 - Fórmula: Volume = Altura * Largura * Comprimento
                 - Exemplo de saída: O volume da caixa é 30 unidades cúbicas.*/
 
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+            decimal altura = 0;
+            decimal largura = 0;
+            decimal comprimento = 0;
+            decimal result = 0;
+
+            Console.WriteLine("Digite a altura da sua caixa retângular: ");
+            altura = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite a largura da sua caixa retângular: ");
+            largura = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Digite o comprimento da sua caixa retângular: ");
+            comprimento = Convert.ToDecimal(Console.ReadLine());
+
+
+            result = altura * largura * comprimento;
+
+            Console.WriteLine("O volume da sua caixa retângular é " + result + " unidades cúbicas.");
+            
         }
     }
 }
