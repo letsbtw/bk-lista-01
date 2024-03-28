1fb26cd816436e601fe9c2ec41f2b6437e97a25b
Leticia Cavalheiro <leticiascavalheiro@gmail.com>
2024-03-28 20:38:38 -0300

    Exercício 04.

---
+++ src/ListaDeExercicios/Exercicio04/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercicio04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /***Calcular o salário total de um vendedor.**
+            - Exemplo de entrada: Salário base = R$ 1500, Total de vendas = R$ 5000, Comissão (5% sobre as vendas)
+            - Fórmula: Salário total = Salário base + (Total de vendas * Comissão)
+            - Exemplo de saída: O salário total do vendedor é R$ 2750.
+            */
+
+            decimal startWage = 0;
+            decimal salesTotal = 0;
+            decimal comission = 0;
+            decimal total = 0;
+
+            Console.WriteLine("Qual o salário inicial?");
+            startWage = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Qual foi o total de vendas?");
+            salesTotal = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Qual a porcentagem de comissão?");
+            comission = Convert.ToDecimal(Console.ReadLine());
+
+            total = startWage + ((salesTotal * comission)/100);
+            Console.WriteLine("O salário total do vendedor é R$ " + total);
+
+
+        }
+    }
+}
