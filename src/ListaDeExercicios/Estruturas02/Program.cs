af1e1f7aa5ea6d3125169b74d8166a4c13747999
Leticia Cavalheiro <leticiascavalheiro@gmail.com>
2024-04-01 20:43:33 -0300

    Estrutura 02.

---
+++ src/ListaDeExercicios/Estruturas02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Estruturas02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            **Calcular o fatorial de um número e exibir o resultado.**
+               - Exemplo de entrada: Número = 5
+               - Exemplo de saída: 5! = 120.
+            */
+            int j = 0;
+            
+
+            Console.WriteLine("Digite o número o qual deseja calcular o fatorial: ");
+            j = Convert.ToInt16(Console.ReadLine());
+            int fat = j;
+
+            for (int i = (j-1); i >1 ; i--)
+            {
+                fat = fat * i;
+            }
+
+            Console.WriteLine(j + "! = " + fat);
+
+        }
+    }
+}
