48de399eb1f55555cfea686a6f1176764620696a
Leticia Cavalheiro <leticiascavalheiro@gmail.com>
2024-04-01 20:06:41 -0300

    Estrutura 01.

---
+++ src/ListaDeExercicios/Estruturas01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Estruturas01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            **Gerar e escrever os números ímpares entre 100 e 200.**
+            - Exemplo de saída: 101, 103, 105, ..., 199.
+            */
+
+            for (int i = 101; i < 200; i++) { 
+                Console.WriteLine(i);
+                i = i + 1;
+
+            }
+        }
+    }
+}
