0303c5f4cb462fdbbfbeec11c85642d5c592539c
Leticia Cavalheiro <leticiascavalheiro@gmail.com>
2024-04-07 19:56:13 -0300

    atualizaçao de nomes das pastas

---
+++ src/ListaDeExercicios/Exercicio05/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Exercicio05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            **Calcular a média ponderada de duas provas realizadas por um aluno.**
+           - Exemplo de entrada: Nota da primeira prova = 7, Peso da primeira prova = 0.4, Nota da segunda prova = 8, Peso da segunda prova = 0.6
+           - Fórmula: Média ponderada = (Nota da primeira prova * Peso da primeira prova) + (Nota da segunda prova * Peso da segunda prova)
+           - Exemplo de saída: A média ponderada do aluno é 7.6.
+
+            */
+
+            decimal grade1 = 0;
+            decimal grade2 = 0;
+            decimal peso1 = 0;
+            decimal peso2 = 0;
+            decimal total = 0;
+
+
+            Console.WriteLine("Qual foi a nota da primeira prova?");
+            grade1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Qual foi o peso da primeira prova?");
+            peso1 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Qual foi a nota da segunda prova?");
+            grade2 = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Qual foi o peso da segunda prova?");
+            peso2 = Convert.ToDecimal(Console.ReadLine());
+        
+            total = (grade1 * peso1) + (grade2 * peso2);
+            
+
+            Console.WriteLine("A nota do aluno por média ponderada será " + total);
+        }
+    }
+}
---
+++ src/ListaDeExercicios/Exercicio09/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection.Emit;
+
+namespace Estruturas03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                    **Imprimir a sequência de Fibonacci até um número informado.**
+           - Exemplo de entrada: Número = 50
+           - Exemplo de saída: Sequência de Fibonacci até 50: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
+            */
+
+            int j = 0;
+            Console.WriteLine("Digite um número: ");
+            j = Convert.ToInt32(Console.ReadLine());
+            int i = 2;
+            int fib = 0;
+            int a = 0;
+            int b = 1;
+
+            Console.Write("0, 1, ");
+            do
+            {
+                fib = a + b;
+                a = b;
+                b = b++;
+                Console.Write(fib);
+            } while (fib < j);
+           
+            
+        }
+    }
+}
