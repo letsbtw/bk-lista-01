466d977ad84d43cc63d94cce47f2ab55166819db
Leticia Cavalheiro <leticiascavalheiro@gmail.com>
2024-04-09 10:34:03 -0300

    Exercício 12.

---
+++ src/ListaDeExercicios/Exercicio12/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercicio12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 1. **Gerar e escrever os números ímpares entre 100 e 200.**
+                   - Exemplo de saída: 101, 103, 105, ..., 199.
+             */
+
+               for (int i = 101; i < 198; i++) {
+                Console.Write(i + ", ");
+                i++;
+                }
+
+               Console.Write("199.");
+        }
+    }
+}
