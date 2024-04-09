298d4c35bd02eb95dcfc394d1bf75c13ad7106b5
Leticia Cavalheiro <leticiascavalheiro@gmail.com>
2024-04-09 10:54:39 -0300

    Exercício 15.

---
+++ src/ListaDeExercicios/Exercicio15/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Exercicio15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 
+                4. **Substituir múltiplos de 3 pela palavra "Fizz" e múltiplos de 5 pela palavra "Buzz" na contagem de 1 a 100.**
+                Exemplo de saída: 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, ...
+             */
+
+            bool fizz = false;
+            bool buzz = false;
+            bool fizzBuzz = false;
+            
+            for (int i = 1; i <= 100; i++) {
+
+                fizz = i % 3 == 0 ? true : false;
+                buzz = i % 5 == 0 ? true : false;
+                fizzBuzz = i % 3 == 0 && i % 5 == 0 ? true : false;
+
+
+                if(fizz)
+                {
+                    if(fizzBuzz)
+                    {
+                        Console.Write("FizzBuzz ");
+                    }
+                    else
+                    {
+                        Console.Write("Fizz ");
+                    }
+                } else if(buzz)
+                {
+                    if (fizzBuzz)
+                    {
+                        Console.Write("FizzBuzz ");
+                    }
+                    else
+                    {
+                        Console.Write("Buzz ");
+                    }
+                } else
+                {
+                    Console.Write(i + " ");
+                }
+
+            }
+
+
+        }
+    }
+}
