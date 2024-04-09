14f07fd9aa1923bf9e3c5368b0b173e195ba6411
Leticia Cavalheiro <leticiascavalheiro@gmail.com>
2024-04-09 10:14:27 -0300

    Exercício 10.

---
+++ src/ListaDeExercicios/Exercicio10/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * 4. **Identificar se um número é par ou ímpar.**
+                - Exemplo de entrada: Número = 7
+                - Exemplo de saída: O número é ímpar.
+            */
+
+            int numero = 0;
+            bool par = false;
+
+            Console.WriteLine("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            par = (numero % 2) == 0 ? true : false;
+
+            if (par)
+            {
+                Console.WriteLine(numero + " é par.");
+            } else
+            {
+                Console.WriteLine(numero + " é ímpar.");
+            }
+
+
+
+        }
+    }
+}
