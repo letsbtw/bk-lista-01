aca8472234db94557e4a66ab5daddda14b1eb6f4
Leticia Cavalheiro <leticiascavalheiro@gmail.com>
2024-04-09 11:13:32 -0300

    Exercício 2.

---
+++ src/ListaDeExercicios/Exercicio08/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exercicio08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * **Ordenar três valores inteiros e diferentes em ordem decrescente.**
+                - Exemplo de entrada: 5, 10, 3
+                - Exemplo de saída: Os números em ordem decrescente são 10, 5, 3.
+             */
+
+            int[] ordem= new int[3];
+            int[] decrescent= new int[3];
+ 
+   
+
+            for (int i=0;i <= 2; i++){
+                Console.WriteLine("Digite um número: ");
+                ordem[i]= Convert.ToInt16(Console.ReadLine());
+            }
+            for (int i = 2; i >= 0; i--)
+            {
+
+                if ((ordem[i] > ordem[0] && ordem[i] > ordem[1]) || (ordem[i] > ordem[0] && ordem[i] > ordem[2]) || (ordem[i] > ordem[1] && ordem[i] > ordem[2]))
+                {
+                    decrescent[0] = ordem[i];
+                }
+                if ((ordem[i] > ordem[0] && ordem[i] < ordem[1]) || (ordem[i] < ordem[0] && ordem[i] > ordem[1]) || (ordem[i] > ordem[0] && ordem[i] < ordem[2]) || (ordem[i] < ordem[0] && ordem[i] > ordem[2] || (ordem[i] > ordem[1] && ordem[i] < ordem[2]) || (ordem[i] < ordem[1] && ordem[i] > ordem[2])))
+                {
+                    decrescent[1] = ordem[i];
+                }
+                if ((ordem[i] < ordem[0] && ordem[i] < ordem[1]) || (ordem[i] < ordem[0] && ordem[i] < ordem[2]) || (ordem[i] < ordem[1] && ordem[i] < ordem[2]))
+                {
+                    decrescent[2] = ordem[i];
+                }
+            }
+            Console.Write(decrescent[0] + " " + decrescent[1] + " " + decrescent[2]);
+
+        }
+    }
+}
