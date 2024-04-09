153cb8c57d0256d1fcbf7639024677c9ee9a218f
Leticia Cavalheiro <leticiascavalheiro@gmail.com>
2024-04-09 10:20:55 -0300

    Exercício 11.

---
+++ src/ListaDeExercicios/Exercicio11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.Serialization;
+
+namespace Exercicio11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             **Realizar uma operação com base em dois valores inteiros A e B.**
+               - Exemplo de entrada: A = 3, B = 3
+               - Exemplo de saída: A + B = 6.
+             */
+            int A = 0;
+            int B = 0;
+            int soma = 0;
+
+            Console.WriteLine("Digite um número: ");
+            A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite outro número: ");
+            B = Convert.ToInt32(Console.ReadLine());
+
+            soma = A + B;
+
+            Console.WriteLine("O resultado da soma de A e B é: " + soma);
+                    
+
+        }
+    }
+}
